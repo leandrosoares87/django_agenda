78cf6127f10c1465540e91448e4590656c5e838a
Leandro Soares <leandrosoaresdev@gmail.com>
2023-11-05 10:28:51 -0300

    configurando settings com as constantes BASE_DIR  e STATICFILES_DIRS e  passando o nome contact para a contante INTALLED_APPS

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,4 @@
+# type: ignore
 """
 Django settings for project project.
 
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'base_static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
