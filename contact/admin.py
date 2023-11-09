c10fe2be0370d1f129947abb4d46b49a2ca626be
Leandro Soares <leandrosoaresdev@gmail.com>
2023-11-09 07:41:43 -0300

    ForeinKey - criando um model category

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'fist_name', 'last_name', 'phone',
+    ordering = '-id',
+    list_filter = 'created_date',
+    search_fields = 'fist_name', 'last_name', 'phone',
+    list_per_page = 1
+    list_max_show_all = 200
+    list_editable = 'last_name', 'phone'
+    list_display_links = 'id', 'fist_name',
--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 
 class Contact(models.Model):
     fist_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     phone = models.CharField(max_length=40)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+
+    def __str__(self) -> str:
+        return f'{self.fist_name} {self.last_name}'
