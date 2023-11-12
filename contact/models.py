5bc82c3a827963db23dc050479b9806b62bce88f
Leandro Soares <leandrosoaresdev@gmail.com>
2023-11-12 10:54:18 -0300

    criando a classe Category em Models

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     fist_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
@@ -11,6 +22,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
-
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
+    
     def __str__(self) -> str:
         return f'{self.fist_name} {self.last_name}'
